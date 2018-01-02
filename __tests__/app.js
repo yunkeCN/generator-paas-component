afb45bc30e482e5695c4a5fd99957bbc6fd55658
熊杰 <285516193@qq.com>
2018-01-02 16:22:00 +0800

    添加spec

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,13 +5,23 @@ const helpers = require('yeoman-test');
 
 describe('generator-paas-component:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      name: 'awesome_name',
+      description: 'awesome component description'
+    });
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.jsonFileContent('package.json', {
+      name: 'awesome_name',
+      description: 'awesome component description',
+      version: '0.0.1',
+      main: 'index.js',
+      scripts: {
+        test: 'echo "Error: no test specified" && exit 1'
+      },
+      author: '',
+      license: 'ISC'
+    });
   });
 });
