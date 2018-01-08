07e5cf1c804875cb9204897672ae39e121bf7381
蒋津 <jiangj01@mysoft.com.cn>
2018-01-08 11:57:12 +0800

    add test

--- __tests__/eslint.js
+++ __tests__/eslint.js
@@ -4,18 +4,7 @@ const helpers = require('yeoman-test');
 describe('node:eslint', () => {
   it('fill package.json', () => {
     return helpers.run(require.resolve('../generators/eslint')).then(() => {
-      assert.fileContent('package.json', /"eslint-config-xo":/);
-      assert.jsonFileContent('package.json', {
-        eslintConfig: {
-          extends: ['xo', 'prettier'],
-          env: {
-            jest: true
-          }
-        },
-        scripts: {
-          pretest: 'eslint .'
-        }
-      });
+      assert.fileContent('package.json', /"eslint-config-airbnb-base":/);
       assert.file('.eslintignore');
     });
   });
